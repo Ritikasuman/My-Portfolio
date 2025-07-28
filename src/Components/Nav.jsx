a2c16a195d1246319fc8dbc269f2f3da0cb201c7
Ritika <ritikasuman56@gmail.com>
2025-07-29 01:35:21 +0530

    'edit'

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -12,18 +12,34 @@ const Nav = () => {
         <div className="flex items-center justify-between">
           {/* Logo */}
           <div>
-            <img src={img} alt="Sign" className="w-44 h-10 sm:w-60 sm:h-12 rounded-full" />
+            <img
+              src={img}
+              alt="Sign"
+              className="w-44 h-10 sm:w-60 sm:h-12 rounded-full"
+            />
           </div>
 
           {/* Desktop Nav Links */}
           <div className="hidden md:flex space-x-8 items-center">
-            <a href="#" className="hover:text-cyan-400 font-bold font-lucid">Home</a>
-            <a href="#about" className="hover:text-cyan-400 font-bold font-sans">About</a>
-            <a href="#contact" className="hover:text-cyan-400 font-bold font-sans">Contact</a>
+            <a href="#" className="hover:text-cyan-400 font-bold font-lucid">
+              Home
+            </a>
             <a
-              href="/Ritika_Suman_CV.pdf"
-              download="RitikaSuman.docx"
-              className="bg-cyan-600/60 rounded-[4vh] px-4 py-1 text-white font-semibold hover:bg-cyan-700 transition duration-300"
+              href="#about"
+              className="hover:text-cyan-400 font-bold font-sans"
+            >
+              About
+            </a>
+            <a
+              href="#contact"
+              className="hover:text-cyan-400 font-bold font-sans"
+            >
+              Contact
+            </a>
+          <a
+              href="/RitikasumanResume.pdf"
+              download
+              className="bg-cyan-600/60 rounded-[4vh] px-4 py-1 text-white font-semibold hover:bg-cyan-700 transition duration-300 w-max"
             >
               Download CV
             </a>
@@ -41,9 +57,15 @@ const Nav = () => {
         {/* Mobile Nav Links */}
         {isOpen && (
           <div className="flex flex-col space-y-4 mt-4 md:hidden items-center">
-            <a href="#" className="hover:text-cyan-400 font-bold">Home</a>
-            <a href="#about" className="hover:text-cyan-400 font-bold">About</a>
-            <a href="#contact" className="hover:text-cyan-400 font-bold">Contact</a>
+            <a href="#" className="hover:text-cyan-400 font-bold">
+              Home
+            </a>
+            <a href="#about" className="hover:text-cyan-400 font-bold">
+              About
+            </a>
+            <a href="#contact" className="hover:text-cyan-400 font-bold">
+              Contact
+            </a>
             <a
               href="/RitikasumanResume.pdf"
               download
